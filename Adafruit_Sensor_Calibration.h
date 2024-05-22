1a4c417c6dd996ee54d0a2b0ea2cfbd32bb80c5e
Abul Al Arabi <68565388+abulalarabi@users.noreply.github.com>
2024-05-22 10:21:43 -0500

    Update Adafruit_Sensor_Calibration.h

--- Adafruit_Sensor_Calibration.h
+++ Adafruit_Sensor_Calibration.h
@@ -4,7 +4,7 @@
 #include <Adafruit_Sensor.h>
 
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__) ||              \
-    defined(__AVR_ATmega2560__) || defined(ESP8266) || defined(ESP32) ||       \
+    defined(__AVR_ATmega2560__) ||       \
     defined(TEENSYDUINO)
 #define ADAFRUIT_SENSOR_CALIBRATION_USE_EEPROM
 #else
