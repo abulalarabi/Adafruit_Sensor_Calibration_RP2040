8d65b1b1a4318171a715fa9b55556ec9b4951d77
Al Arabi <arabiabulal@gmail.com>
2024-09-25 05:20:00 -0500

    fix

--- Adafruit_Sensor_Calibration_SDFat.h
+++ Adafruit_Sensor_Calibration_SDFat.h
@@ -1,173 +1,45 @@
+#ifndef __ADAFRUIT_SENSOR_CALIBRATION_SDFAT__
+#define __ADAFRUIT_SENSOR_CALIBRATION_SDFAT__
+
 #include "Adafruit_Sensor_Calibration.h"
-#include <LittleFS.h>
+
 #if defined(ADAFRUIT_SENSOR_CALIBRATION_USE_SDFAT)
 
-#ifdef ADAFRUIT_SENSOR_CALIBRATION_USE_FLASH
-#ifdef ADAFRUIT_SENSOR_CALIBRATION_USE_QSPIFLASH
-static Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS,
-                                                   PIN_QSPI_IO0, PIN_QSPI_IO1,
-                                                   PIN_QSPI_IO2, PIN_QSPI_IO3);
-#elif defined(ADAFRUIT_SENSOR_CALIBRATION_USE_SPIFLASH)
-static Adafruit_FlashTransport_SPI flashTransport(EXTERNAL_FLASH_USE_CS,
-                                                  &EXTERNAL_FLASH_USE_SPI);
+// Use filesystem! Must be SdFat derived
+#include <SdFat.h>
+// We'll need Arduino JSON to store in a file
+#include <ArduinoJson.h>
+
+#if defined(EXTERNAL_FLASH_DEVICES) || defined(PIN_QSPI_SCK)
+#include <Adafruit_SPIFlash.h>
+#define ADAFRUIT_SENSOR_CALIBRATION_USE_FLASH
+
+#if defined(EXTERNAL_FLASH_USE_QSPI) || defined(PIN_QSPI_SCK)
+#define ADAFRUIT_SENSOR_CALIBRATION_USE_QSPIFLASH
+#elif defined(EXTERNAL_FLASH_USE_SPI)
+#define ADAFRUIT_SENSOR_CALIBRATION_USE_SPIFLASH
+#else
+#error("Chip has external flash chip but no interface defined in variant!")
 #endif
 
-static Adafruit_SPIFlash flash(&flashTransport);
-static FatFileSystem fatfs;
 #endif
 
-Adafruit_Sensor_Calibration_SDFat::Adafruit_Sensor_Calibration_SDFat() {}
+/**!  @brief Class for managing storing calibration in external flash or
+   SDFat-compatible storage **/
+class Adafruit_Sensor_Calibration_SDFat : public Adafruit_Sensor_Calibration {
+public:
+  Adafruit_Sensor_Calibration_SDFat();
+  bool begin(const char *filename = NULL, FatFileSystem *filesys = NULL);
+  bool printSavedCalibration(void);
+  bool loadCalibration(void);
+  bool saveCalibration(void);
 
-/**************************************************************************/
-/*!
-    @brief Initializes Flash and/or filesystem
-    @param filename The filename we expect to find calibration in, defaults
-    to "calib.json" if not supplied
-    @param filesys The optional, external filesystem we'll use to access. If
-    not provided, we look for an internal (Q)SPI Flash memory
-    @returns False if any failure to initialize flash or filesys
-*/
-/**************************************************************************/
-bool Adafruit_Sensor_Calibration_SDFat::begin(const char *filename,
-                                              FatFileSystem *filesys) {
-  // Initialize the filesystem with LittleFS LittleFS.begin();
-  bool ec = LittleFS.begin();
-
-  if(!ec) {
-    Serial.println("Failed to mount filesystem!");
-    return false;
-  }
-  Serial.println("Mounted filesystem!");
-
-  //File root = LittleFS.open("sensor_calib.json");
-  char file[80];
-  
-
-
-  if (filename) {
-    _cal_filename = filename;
-  } else {
-    _cal_filename = "sensor_calib.json";
-  }
-  return true;
-}
-
-/**************************************************************************/
-/*!
-    @brief Save the calibration file and serialize this object's calibrations
-    into JSON format
-    @returns false if anything went wrong with opening the file
-*/
-/**************************************************************************/
-bool Adafruit_Sensor_Calibration_SDFat::saveCalibration(void) {
-  // if (!theFS)
-  //   return false;
-
-  File file = LittleFS.open(_cal_filename, "w");
-  if (!file) {
-    Serial.println(F("Failed to create file"));
-    return false;
-  }
-
-  JsonObject root = calibJSON.to<JsonObject>();
-  JsonArray mag_hard_data = root.createNestedArray("mag_hardiron");
-  for (int i = 0; i < 3; i++) {
-    mag_hard_data.add(mag_hardiron[i]);
-  }
-  JsonArray mag_soft_data = root.createNestedArray("mag_softiron");
-  for (int i = 0; i < 9; i++) {
-    mag_soft_data.add(mag_softiron[i]);
-  }
-  root["mag_field"] = mag_field;
-  JsonArray gyro_zerorate_data = root.createNestedArray("gyro_zerorate");
-  for (int i = 0; i < 3; i++) {
-    gyro_zerorate_data.add(gyro_zerorate[i]);
-  }
-  JsonArray accel_zerog_data = root.createNestedArray("accel_zerog");
-  for (int i = 0; i < 3; i++) {
-    accel_zerog_data.add(accel_zerog[i]);
-  }
-  // serializeJsonPretty(root, Serial);
-
-  // Serialize JSON to a buffer
-  char buffer[1024];
-  size_t n = serializeJsonPretty(root, buffer);
-  file.write((const uint8_t *)buffer, n);
-
-  
-  // Close the file (File's destructor doesn't close the file)
-  file.close();
-
-  return true;
-}
-
-/**************************************************************************/
-/*!
-    @brief Print the raw calibration file/EEPROM data
-    @returns false if anything went wrong with opening the file
-*/
-/**************************************************************************/
-bool Adafruit_Sensor_Calibration_SDFat::printSavedCalibration(void) {
-  
-  File file = LittleFS.open(_cal_filename, "r");
-  if (!file) {
-    Serial.println(F("Failed to read file"));
-    return false;
-  }
-
-  Serial.println("------------");
-  while (file.available()) {
-    Serial.write(file.read());
-  }
-  Serial.println("\n------------");
-  file.close();
-  yield();
-  return true;
-}
-
-/**************************************************************************/
-/*!
-    @brief Load the calibration file and parse JSON into this object
-    @returns false if anything went wrong with opening the file
-*/
-/**************************************************************************/
-bool Adafruit_Sensor_Calibration_SDFat::loadCalibration(void) {
-
-  File file = LittleFS.open(_cal_filename, "r");
-  if (!file) {
-    Serial.println(F("Failed to read file"));
-    return false;
-  }
-
-  // Deserialize the JSON document
-  DeserializationError error = deserializeJson(calibJSON, file);
-  if (error) {
-    Serial.println(F("Failed to read file"));
-    return false;
-  }
-
-  // Close the file (File's destructor doesn't close the file)
-  file.close();
-
-  for (int i = 0; i < 3; i++) {
-    mag_hardiron[i] = calibJSON["mag_hardiron"][i] | 0.0;
-  }
-  for (int i = 0; i < 9; i++) {
-    float def = 0;
-    if (i == 0 || i == 4 || i == 8) {
-      def = 1;
-    }
-    mag_softiron[i] = calibJSON["mag_softiron"][i] | def;
-  }
-  mag_field = calibJSON["mag_field"] | 0.0;
-  for (int i = 0; i < 3; i++) {
-    gyro_zerorate[i] = calibJSON["gyro_zerorate"][i] | 0.0;
-  }
-  for (int i = 0; i < 3; i++) {
-    accel_zerog[i] = calibJSON["accel_zerog"][i] | 0.0;
-  }
-
-  return true;
-}
+private:
+  FatFileSystem *theFS = NULL;
+  const char *_cal_filename = NULL;
+  StaticJsonDocument<512> calibJSON;
+};
 
 #endif
+
+#endif // include once
